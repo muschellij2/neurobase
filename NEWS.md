d118aaa2611c74d6ffeafe12e2546711f1705a67
muschellij2 <muschellij2@gmail.com>
2016-09-20 16:52:51 -0400

    added all

---
+++ NEWS.md
@@ -0,0 +1,20 @@
+# fslr v1.6.0 (Release date: 2015-12-01)
+
+## Major changes
+
+* Changed default reading/writing to `readnii`/`writenii`
+
+# fslr v1.5.1 (Release date: 2015-10-20)
+
+## Major changes
+
+* Added `readnii` and `writenii`
+* Added `ortho_diff`
+
+# fslr v1.4-4 (Release date: 2015-05-20)
+
+
+## Major Changes
+
+* Moved functions such as cal_img to oro.nifti package.  They are all legacies, such as cal_img still works, but calibrateImage and other camelCase functions have been made default.
+* Fixed fsl_biascorrect.  
---
+++ R/cog.R
@@ -0,0 +1,44 @@
+
+#' @title Image Center of Gravity
+#' @description Find Center of Gravity of Image, after thresholding
+#' @param img Object of class nifti
+#' @param thresh threshold for image, will find \code{img > 0}
+#' @param ceil Run \code{\link{ceiling}} to force integers (usu for plotting)
+#' @param warn Produce a warning if the image is empty after thresholding
+#' @return Vector of length 3
+#' @export
+#' @examples
+#' x = array(rnorm(1e6), dim = c(100, 100, 100))
+#' img = nifti(x, dim= c(100, 100, 100), 
+#' datatype = convert.datatype()$FLOAT32, cal.min = min(x), 
+#' cal.max = max(x), pixdim = rep(1, 4))
+#' cog(img)
+cog = function(img, thresh = 0, ceil = FALSE, warn = TRUE){
+  #   stopifnot(inherits(img, "nifti"))
+  mask = img > thresh
+  if (sum(mask, na.rm = TRUE) == 0) {
+    if (warn) {
+      warning(paste0("No voxels found to be > ", round(thresh, 3), 
+                     ", using the center of whole image"))
+    }
+    xyz = dim(mask) / 2
+  } else {
+    xyz = colMeans(which(mask, arr.ind = TRUE))
+  }
+  if (ceil) xyz = ceiling(xyz)
+  return(xyz)
+}
+
+
+
+#' @title Image Center of Gravity Wrapper
+#' @description Find Center of Gravity of Image, after thresholding and
+#' take ceiling (wrapper for \code{\link{cog}})
+#' @param ... Arguments ppssed to \code{\link{cog}}
+#' @return Vector of length 3
+#' @note Just a convenience wrapper for \code{cog(ceil=TRUE)}
+#' @export
+xyz = function(...){
+  xyz = cog(..., ceil = TRUE)
+  return(xyz)
+}
