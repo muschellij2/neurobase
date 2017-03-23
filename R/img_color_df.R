1a01da214c7aec5b61f5a769d1f82f6a93dd0981
muschellij2 <muschellij2@gmail.com>
2017-03-23 11:39:01 -0400

    Added img_colour_df

---
+++ R/img_color_df.R
@@ -0,0 +1,53 @@
+#' @title Convert Image to Data.frame with Colors
+#' @description Takes in an image and a color scheme, converts that image
+#' into a \code{data.frame} with the data and a color mapping.
+#'
+#' @param img an object to be coerced to \code{nifti} using 
+#' \code{\link{check_nifti}}
+#' @param zlim Limits for the domain of the intensities
+#' @param breaks Breaks for the intensities to map to colors
+#' @param col Colors to map intensities
+#'
+#' @return A \code{data.frame} with the first columns being the x,y,z (maybe t)
+#' coordinates (named \code{dim} and the dimension number), a \code{value} 
+#' column that contains the intensity information, and a \code{colour} column
+#' representing the color that voxel maps to
+#' @export
+#'
+#' @examples
+#' img = nifti(array(rnorm(10^3), dim = rep(10, 3)))
+#' df = img_colour_df(img)
+img_colour_df = function(
+  img,
+  zlim = NULL,
+  breaks = NULL,
+  col = gray(0:64/64)) {
+  img = check_nifti(img)
+  
+  zlim = zlimmer(img, zlim = zlim)
+  if (is.null(breaks)) {
+    breaks <- c(min(img, zlim, na.rm = TRUE),
+                seq(min(zlim, na.rm = TRUE),
+                    max(zlim, na.rm = TRUE),
+                    length = length(col) - 1),
+                max(img, zlim, na.rm = TRUE))
+  }
+  
+  dimg = dim(img)
+  L = lapply(dimg, seq)
+  eg = expand.grid(L)
+  colnames(eg) = paste0("dim", seq(ncol(eg)))
+  eg$value = img[as.matrix(eg)]
+  eg$zi = .bincode(eg$value, breaks, TRUE, TRUE) - 1L
+  eg$colour = col[ eg$zi + 1]
+  
+  return(eg)
+}
+
+#' @rdname img_colour_df
+#' @param ... not used
+#' @note \code{img_color_df} is a duplicate of \code{img_colour_df}
+#' @export
+img_color_df = function(...) {
+  img_colour_df(...)
+}
---
+++ R/slice_colour_df.R
@@ -0,0 +1,49 @@
+#' @title Slice a Image Color Data.frame 
+#' @description Slices a image color \code{data.frame} along the 3 planes (axial,
+#' coronal, sagittal) and returns it in a ggplot-ready format for faceting.
+#'
+#' @param img_df an image \code{data.frame}, usually from 
+#' \code{\link{img_colour_df}}.  Must have the columns: \code{dim1},
+#' \code{dim2}, \code{dim3}, \code{colour}, and \code{value}.
+#' @param xyz coordinates to slice the \code{data.frame} in x, y, and z - domains
+#'
+#' @return A \code{data.frame} with x and y coordinates, colour, and intensity
+#' values, along with the associated planes that were sliced.
+#' @export
+#'
+#' @examples
+#' img = nifti(array(rnorm(10^3), dim = rep(10, 3)))
+#' df = img_colour_df(img)
+#' sliced = slice_colour_df(df, c(5, 5, 4))
+slice_colour_df = function(
+  img_df, 
+  xyz) {
+  
+  v3 = img_df[ img_df$dim3 == xyz[3],
+               c("dim1", "dim2", "colour", "value")]
+  v2 = img_df[ img_df$dim2 == xyz[2],
+               c("dim1", "dim3", "colour", "value")]
+  v1 = img_df[ img_df$dim1 == xyz[1],
+               c("dim2", "dim3", "colour", "value")]
+  v1$plane = 1
+  v2$plane = 2
+  v3$plane = 3
+  
+  L = list(v1, v2, v3)
+  L = lapply(L, function(x){
+    colnames(x) = c("x", "y", "colour", "value", "plane")
+    x
+  })
+  L = do.call("rbind", L)
+  L = as.data.frame(L)
+  L$plane2 = L$plane
+  L$plane2[ L$plane == 2 ] = "coronal"
+  L$plane2[ L$plane == 3 ] = "axial"
+  L$plane2[ L$plane == 1 ] = "sagittal"
+  
+  L$plane2 = factor(
+    L$plane2,
+    levels = c("coronal", "sagittal", "axial"))
+  return(L)
+}
+
