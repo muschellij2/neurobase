c785f101c053390fdead831104a2e0ad94ab66ef
muschellij2 <muschellij2@gmail.com>
2017-03-30 16:04:17 -0400

    Updated with warnings and on the cut.

--- R/cut.R
+++ R/cut.R
@@ -3,19 +3,25 @@
 #'  with the option of a mask.
 #' 
 #' @param x Object of class \code{nifti}
+#' @param breaks either a numeric vector of two or more unique cut points 
+#' or a single number (greater than or equal to 2) 
+#' giving the number of intervals into which x is to be cut. 
+#' Passed to \code{\link{cut}})
 #' @param ... Arguments passed to \code{\link{cut}}
 #' @param mask object to subset the image.  If missing, then all 
 #' values of the image are used
 #'
-#' @return Object of \class{nifti} with an \code{attribute} of levels
+#' @return Object of class \code{nifti} with an \code{attribute} of levels
 #' @export
 #' @examples 
 #' img = nifti(array(rnorm(10^3), dim = rep(10, 3)))
 #' mask = img > 0
-#' cut(img, mask = mask)
+#' cut(img, mask = mask, breaks = 4)
 cut.nifti = function(x, 
+                     breaks,
                      ...,
                      mask){
+  ximg = x
   if (missing(mask)) {
     x = img_data(x)
     x = c(x)
@@ -23,17 +29,16 @@ cut.nifti = function(x,
   } else {
     x = mask_vals(object = x, mask)
   }
-  cuts = cut(x, ...)
+  cuts = cut(x, breaks = breaks, ...)
   levs = levels(cuts)
   cuts = as.numeric(cuts)
   
-  x = remake_img(vec = cuts, img = x, mask = mask)
+  x = remake_img(vec = cuts, img = ximg, mask = mask)
   x = mask_img(x, mask = mask)
   
   attr(x, "levels") = levs
   return(x)
 } 
-
 
 
 #' @export
