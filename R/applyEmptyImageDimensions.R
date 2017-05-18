ea1eee860bf441d3c6c0affe3f0e1568f72a5743
muschellij2 <muschellij2@gmail.com>
2017-05-18 09:54:38 -0400

    Making applyEmptyImageDimensions generic.

--- R/applyEmptyImageDimensions.R
+++ R/applyEmptyImageDimensions.R
@@ -1,11 +1,14 @@
+#' @name applyEmptyImageDimensions-methods
+#' @docType methods 
+#' @aliases applyEmptyImageDimensions 
 #' @title Apply Subsetting from Empty Image Dimensions
-#' @name applyEmptyImageDimensions
-#' @param img nifti object
+#' @description Simple wrapper for subsetting an image with indices, 
+#' dropping empty dimensions.  
+#' @param img image, nifti object, or array
 #' @param inds indices of subset from \code{\link{getEmptyImageDimensions}} or
 #' \code{\link{dropEmptyImageDimensions}}.
 #' @param reorient Should image be reoriented if a filename?
-#' @description Simple wrapper for subsetting an image with indices, 
-#' dropping empty dimensions.
+#' @param ... not used
 #' @return Object of class \code{nifti} or \code{array} if \code{nifti}
 #' is not supplied
 #' @note \code{apply_empty_dim} is a shorthand for 
@@ -13,31 +16,120 @@
 #' @seealso \code{\link{getEmptyImageDimensions}}, 
 #' \code{\link{dropEmptyImageDimensions}} 
 #' @export
-applyEmptyImageDimensions <- function(img, 
-                                      inds,
-                                      reorient = FALSE) {
-  
-  img = check_nifti(img, reorient = reorient, allow.array = TRUE)
+setGeneric("applyEmptyImageDimensions", 
+           function(img, 
+                    inds,
+                    reorient = FALSE,
+                    ...) {
+             standardGeneric("applyEmptyImageDimensions")
+           }) 
+
+.applyEmptyImageDimensions <- function(img, 
+                                       inds,
+                                       reorient = FALSE) {
   dimg = dim(img)
   if (length(dimg) > 3) {
     stop(paste0("Only images with 3 dimensions supported, ", 
                 "as checked by length(dim(img))"))
   }
-  i2 = img[inds[[1]], inds[[2]], inds[[3]]]
-  if (is.nifti(img)) {
-    outimg = copyNIfTIHeader(img = img, arr = i2, drop = TRUE)  
-    return(outimg)
-  } else {
-    return(i2)
-  }
+  i2 = img[inds[[1]], inds[[2]], inds[[3]]] 
+  return(i2)
 }
 
-#' @rdname applyEmptyImageDimensions
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,nifti-method
+#' @export
+setMethod("applyEmptyImageDimensions", "nifti", 
+          function(img, 
+                   inds,
+                   reorient = FALSE) {
+            res = .applyEmptyImageDimensions(
+              img = img, inds = inds, 
+              reorient = reorient)
+            res = copyNIfTIHeader(img = img, 
+                                  arr = res, 
+                                  drop = TRUE)  
+            return(res)
+          })
+
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,nifti-method
+#' @export
+setMethod("applyEmptyImageDimensions", "character", 
+          function(img, ...) {
+            img = check_nifti(img, reorient = reorient)
+            res = applyEmptyImageDimensions(img, ...)
+            return(res)
+          })
+
+
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,factor-method
+#'  
+#' @export
+setMethod("applyEmptyImageDimensions", "factor", 
+          function(img, ...) { 
+            img = as.character(img)
+            res = applyEmptyImageDimensions(img = img, ...)
+            return(res)
+          })
+
+
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,list-method
+#' @export
+setMethod("applyEmptyImageDimensions", "list", 
+          function(img, ...) { 
+            ### add vector capability
+            res = lapply(img, applyEmptyImageDimensions, 
+                          ...)
+            return(res)
+          })
+
+
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,array-method
+#' @export
+setMethod("applyEmptyImageDimensions", "array", 
+          function(img, ...) { 
+            res = .applyEmptyImageDimensions(
+              img = img, ...)            
+            return(res)
+          })
+
+
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,anlz-method
+#' @export
+setMethod("applyEmptyImageDimensions", "anlz", 
+          function(img, ...) { 
+            img = as.nifti(img)
+            res = applyEmptyImageDimensions(img = img, ...)
+            return(res)
+          })
+
+#' @rdname applyEmptyImageDimensions-methods
+#' @aliases applyEmptyImageDimensions,ANY-method
+#' @export
+setMethod("applyEmptyImageDimensions", "ANY", 
+          function(img, ...) {
+            # workaround because can't get class
+            if (inherits(img, "niftiImage")) {
+              img = check_nifti(img)
+              res = applyEmptyImageDimensions(img = img, ...)
+              return(res)              
+            } else {
+              stop("Not implemented for this type!")
+            }
+            return(img)
+          })
+
+
+
+
+#' @rdname applyEmptyImageDimensions-methods
 #' @export
 apply_empty_dim <- function(img, 
-                            inds,
-                            reorient = FALSE) {
-  applyEmptyImageDimensions(img = img, 
-                           inds = inds,
-                           reorient = reorient)
+                            ...) {
+  applyEmptyImageDimensions(img = img, ...)
 }
