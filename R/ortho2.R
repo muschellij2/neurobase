8d5f06e444d422c9c87bef6d7ebbfb4914b3e5af
muschellij2 <muschellij2@gmail.com>
2019-06-10 15:26:59 -0400

    fixing RNifti aspect ratio

--- R/ortho2.R
+++ R/ortho2.R
@@ -194,9 +194,11 @@ ortho2 = function(x, y = NULL, xyz = NULL, w = 1, col = gray(0:64/64),
     pdim = pixdim(x)
   } else {
     if (inherits(x, "niftiImage")) {
-      pdim = pixdim(x)
-      if (length(pdim) <= 3) {
-        pdim = c(pdim, rep(1, 4 - length(pdim)))
+      if (is.null(pdim)) {
+        pdim = c(1, pixdim(x))
+        if (length(pdim) <= 3) {
+          pdim = c(pdim, rep(1, 4 - length(pdim)))
+        }
       }
     }
     if (is.null(pdim)) {
